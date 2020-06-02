64433eb01a4e2afc09c0898e11654d8a6f301ee3
kolimihariprasad <kolimi.hariprasad@gmail.com>
2020-06-03 00:50:47 +0530

    Added: stored result in dictionaries

--- sorting.sh
+++ sorting.sh
@@ -1,13 +1,10 @@
 read -p "enter first number" a
 read -p "enter first number" b
 read -p "enter first number" c
-echo $a $b $c
-result1=$((a+b*c))
-echo $result1
-result2=$((a*b+c))
-echo $result2
-result3=`awk "BEGIN {print $c+$a/$b}"`
-echo $result3
-result4=$((a%b+c))
-echo $result4
-
+declare -A result
+result[result1]=$((a+b*c))
+result[result2]=$((a*b+c))
+result[result3]=`awk "BEGIN {print $c+$a/$b}"`
+result[result4]=$((a%b+c))
+echo ${result[@]}
+echo ${!result[@]}
