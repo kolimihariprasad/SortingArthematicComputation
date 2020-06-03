847ec4cb8f9d90bf50ddc60010e8649fb8be979b
kolimihariprasad <kolimi.hariprasad@gmail.com>
2020-06-03 09:36:19 +0530

    Added: sorted in Ascending order

--- sorting.sh
+++ sorting.sh
@@ -4,11 +4,26 @@ read -p "enter first number" c
 declare -A result
 result[result1]=$((a+b*c))
 result[result2]=$((a*b+c))
-result[result3]=`awk "BEGIN {print $c+$a/$b}"`
+result[result3]=$((c+a/b))
+#result[result3]=`awk "BEGIN {print $c+$a/$b}"`
 result[result4]=$((a%b+c))
-for((i=0;i<5;i++))
+for((k=0;k<5;k++))
 do
-	resultArray[$i]=${result["result"$i]}
+	resultArray[$k]=${result["result"$k]}
 done
-echo ${resultArray[@]} | sort
+echo ${resultArray[@]}
+#echo ${resultArray[@]} | sort -r -n
+for(( i=1;i<5;i++ ))
+do
+        for(( j=$i+1;j<5;j++ ))
+        do
+                if [ ${resultArray[$i]} -gt ${resultArray[$j]} ]
+                then
+                        temp=${resultArray[$i]}
+                        resultArray[$i]=${resultArray[$j]}
+                        resultArray[$j]=$temp
+                fi
+        done
+done
 
+echo ${resultArray[@]}
